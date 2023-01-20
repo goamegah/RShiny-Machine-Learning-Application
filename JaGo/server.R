f89013a1de3b09676d3a03dadc6e4882998320f6
Godwin AMEGAH <winneramegah492@gmail.com>
2023-01-20 22:27:17 +0100

    séparation du projet en 3 fichiers

--- JaGo/server.R
+++ JaGo/server.R
@@ -1,17 +1,59 @@
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-    
-    output$distPlot <- renderPlot({
-        
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-        
+e = new.env()
+
+
+server = function (input, output, session) {
+    donneesTXT <- reactive({
+        if (is.null(input$fichierTXT)) return (NULL)
+        if (input$fichierSep == 'autre') sep = input$fichierSepAutre
+        else sep = input$fichierSep
+        don = NULL
+        try({
+            don = read.table(
+                input$fichierTXT$datapath,
+                header = input$fichierHeader,
+                sep = sep,
+                dec = input$fichierDec,
+                skip = input$fichierSkipNb,
+                stringsAsFactors = FALSE)
+        }, silent = TRUE)
+        don
     })
     
-})
+    observeEvent(input$fichierRDATA, {
+        input$fichierRDATA
+        
+        load(input$fichierRDATA$datapath, envir = e)
+        liste = ls(e)
+        
+        updateSelectInput(session = session, inputId = "choixVar", choices = liste)
+    })
+    
+    donneesRDATA <- reactive({
+        don = NULL
+        try({
+            don = get(input$choixVar, envir = e)
+        }, silent = TRUE)
+        don
+    })
+    
+    donnees <- reactive({
+        don = NULL
+        if (input$choixDF == "Fichier texte") {
+            # Données à charger
+            don = donneesTXT()
+        } else {
+            if (input$choixDF == "Fichier RData") {
+                don = donneesRDATA()
+            } else {
+                don = get(input$choixDF)
+            }
+        }
+        don
+    })
+    
+    output$table <- DT::renderDataTable({
+        donnees()
+    })
+    output$nbLig <- renderText({ nrow(donnees()) })
+    output$nbCol <- renderText({ ncol(donnees()) })
+}
--- JaGo/ui.R
+++ JaGo/ui.R
@@ -1,22 +1,41 @@
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-    
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-    
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+ui = dashboardPage(
+    dashboardHeader(
+        title = "Correction"
+    ),
+    dashboardSidebar(
+        selectInput(
+            "choixDF",
+            label = "Choix des données",
+            choices = c("mtcars", "iris", "LifeCycleSavings", "Fichier texte", "Fichier RData")
         ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        conditionalPanel(
+            condition = "input.choixDF == 'Fichier texte'",
+            fileInput("fichierTXT", label = "Fichier à importer", placeholder = "Choississez votre fichier texte"),
+            checkboxInput("fichierHeader", label = "Noms de variables présents", value = FALSE),
+            radioButtons("fichierSep", label = "Séparateur", 
+                         c("point-virgule" = ";", "virgule" = ",", "espace" = " ", "tabulation" = "\t", "autre" = "autre")),
+            conditionalPanel(
+                condition = "input.fichierSep == 'autre'",
+                textInput("fichierSepAutre", label = "Séparateur")
+            ),
+            radioButtons("fichierDec", label = "Séparateur de décimales",
+                         c("point" = ".", "virgule" = ",")),
+            checkboxInput("fichierSkip", label = "Ignorer les premières lignes ?"),
+            conditionalPanel(
+                condition = "input.fichierSkip",
+                textInput("fichierSkipNb", label = "Nombre de lignes", value = 0)
+            )
+        ),
+        conditionalPanel(
+            condition = "input.choixDF == 'Fichier RData'",
+            fileInput("fichierRDATA", label = "Fichier à importer", placeholder = "Choississez votre fichier RData"),
+            selectInput("choixVar", label = "Quelle variable prendre ?", choices = NULL)
         )
-    )
-))
+    ),
+    dashboardBody(
+        valueBox(textOutput("nbLig"), subtitle = "Lignes", width = 6, icon = icon("grip-lines")),
+        valueBox(textOutput("nbCol"), subtitle = "Colonnes", width = 6, icon = icon("grip-lines-vertical")),
+        DT::dataTableOutput("table")
+    ),
+    title = "Correction"
+)
