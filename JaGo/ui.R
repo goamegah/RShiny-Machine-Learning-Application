9e8bbe64aaf49cbcdf8b26bfecdbfbbda7178d97
Godwin AMEGAH <winneramegah492@gmail.com>
2023-01-22 12:58:10 +0100

    nouvelle section du menu

--- JaGo/ui.R
+++ JaGo/ui.R
@@ -1,37 +1,97 @@
 ui = dashboardPage(
+    
+    # App header 
     dashboardHeader(
-        title = "Correction"
+        title = "JaGo"
     ),
+    
+    # App sidebar
     dashboardSidebar(
-        selectInput(
-            "choixDF",
-            label = "Choix des données",
-            choices = c("mtcars", "iris", "LifeCycleSavings", "Fichier texte", "Fichier RData")
-        ),
-        conditionalPanel(
-            condition = "input.choixDF == 'Fichier texte'",
-            fileInput("fichierTXT", label = "Fichier à importer", placeholder = "Choississez votre fichier texte"),
-            checkboxInput("fichierHeader", label = "Noms de variables présents", value = FALSE),
-            radioButtons("fichierSep", label = "Séparateur", 
-                         c("point-virgule" = ";", "virgule" = ",", "espace" = " ", "tabulation" = "\t", "autre" = "autre")),
+        
+        # sidebar menu
+        sidebarMenu(
+            id = "gauche",
+            menuItem(text = " Dataset ", tabName = "sb_data", icon = icon("database")),
+            
+            # Si on clique sur le boutton dataset
             conditionalPanel(
-                condition = "input.fichierSep == 'autre'",
-                textInput("fichierSepAutre", label = "Séparateur")
+                condition = "input.gauche == 'sb_data'", 
+                selectInput(
+                    inputId = "choixDF",
+                    label = "Choix des données",
+                    choices = c("mtcars", "iris", "LifeCycleSavings", "Fichier texte", "Fichier RData")
+                ),
+                # Si en plus de cela on choisit un fichier texte
+                conditionalPanel(
+                    condition = "input.choixDF == 'Fichier texte'",
+                    
+                    # choix
+                    fileInput(
+                        inputId = "fichierTXT", 
+                        label = "Fichier à importer", 
+                        placeholder = "Choississez votre fichier texte"),
+                    
+                    # entêtes
+                    checkboxInput(
+                        inputId = "fichierHeader", 
+                        label = "Noms de variables présents",
+                        value = FALSE),
+                    
+                    radioButtons(
+                        inputId = "fichierSep",
+                        label = "Séparateur", 
+                        choices = c("point-virgule" = ";", "virgule" = ",", "espace" = " ", "tabulation" = "\t", "autre" = "autre")),
+                    
+                    conditionalPanel(
+                        condition = "input.fichierSep == 'autre'",
+                        textInput(
+                            inputId = "fichierSepAutre",
+                            label = "Séparateur")
+                    ),
+                    
+                    radioButtons(
+                        inputId = "fichierDec", 
+                        label = "Séparateur de décimales",
+                        choices = c("point" = ".", "virgule" = ",")
+                    ),
+                    
+                    checkboxInput(
+                        inputId = "fichierSkip", 
+                        label = "Ignorer les premières lignes ?"
+                    ),
+                    
+                    conditionalPanel(
+                        condition = "input.fichierSkip",
+                        textInput(
+                            inputId = "fichierSkipNb", 
+                            label = "Nombre de lignes", value = 0)
+                    )
+                ),
+                
+                conditionalPanel(
+                    condition = "input.choixDF == 'Fichier RData'",
+                    
+                    fileInput(
+                        inputId = "fichierRDATA", 
+                        label = "Fichier à importer", 
+                        placeholder = "Choississez votre fichier RData"
+                    ),
+                    
+                    selectInput(
+                        inputId = "choixVar", 
+                        label = "Quelle variable prendre ?", 
+                        choices = NULL
+                    )
+                )
             ),
-            radioButtons("fichierDec", label = "Séparateur de décimales",
-                         c("point" = ".", "virgule" = ",")),
-            checkboxInput("fichierSkip", label = "Ignorer les premières lignes ?"),
-            conditionalPanel(
-                condition = "input.fichierSkip",
-                textInput("fichierSkipNb", label = "Nombre de lignes", value = 0)
-            )
-        ),
-        conditionalPanel(
-            condition = "input.choixDF == 'Fichier RData'",
-            fileInput("fichierRDATA", label = "Fichier à importer", placeholder = "Choississez votre fichier RData"),
-            selectInput("choixVar", label = "Quelle variable prendre ?", choices = NULL)
+            
+            # ... A suivre 
+            menuItem(text = " Analyse Exploratoire", tabName = "sb_anal", icon=icon("chart-line")),
+            menuItem(text = " Machine Learning ", tabName = "sb_machine", icon = icon("database"))
         )
     ),
+    
+    # App Body 
     dashboardBody(
         valueBox(textOutput("nbLig"), subtitle = "Lignes", width = 6, icon = icon("grip-lines")),
         valueBox(textOutput("nbCol"), subtitle = "Colonnes", width = 6, icon = icon("grip-lines-vertical")),
